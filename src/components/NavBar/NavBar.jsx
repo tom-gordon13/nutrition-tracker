37d80684dc75140eab3e9ef659da313f422aa1f6
tom-gordon13 <tombcgordon@gmail.com>
2022-06-06 19:01:14 -0600

    Add styling to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,47 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import * as userService from "../../utilities/users-service"
+import './NavBar.css'
 
 export default function NavBar({ user, setUser }) {
+    const [selectedPage, setSelectedPage] = useState('Daily View')
+    
     function handleLogOut() {
         // Remove token using the user service
         userService.logOut();
         // Update user state in App
         setUser(null)
     }
-    
-    
+
+    function handleLinkClick(evt) {
+        setSelectedPage(evt.target.innerText)
+    }
+
     return (
-        <nav>
-            
-            
-            <Link to="/">Day View</Link>
-            &nbsp;&nbsp;
-            {/* <Link to="/food/new">Food Test NEW</Link> */}
-            &nbsp;&nbsp;
-            <span>Hi {user.name}</span>
-            &nbsp;&nbsp;
-            <Link to="" onClick={handleLogOut}>Log Out</Link>
+        <nav className='d-flex'>
+
+            <span className="leftLogo display-3">Nourish</span>
+
+            <div className='navBar-right'>
+                <Link 
+                to="/" 
+                className={`text-decoration-none navBar-link ${selectedPage === 'Daily View' ? "selectedPage" : ""}`} 
+                value='Daily View'
+                onClick={handleLinkClick}
+                >Daily View</Link>
+                &nbsp;&nbsp;
+                <Link 
+                to="/" 
+                className={`text-decoration-none navBar-link ${selectedPage === 'Daily View Two' ? "selectedPage" : ""}`}
+                value='Daily View Two'
+                onClick={handleLinkClick}
+                >Daily View Two</Link>
+                &nbsp;&nbsp;
+                
+                {/* <span>Hi {user.name}</span>
+            &nbsp;&nbsp; */}
+                <Link to="" onClick={handleLogOut} className='text-decoration-none'>Log Out</Link>
+            </div>
         </nav>
     )
 }
